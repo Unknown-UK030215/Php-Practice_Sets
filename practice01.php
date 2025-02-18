ca13a3672d43999c4b03b5a3fc128c0ad22ed662
Unknown-UK030215 <santosmarkvincent15@gmail.com>
2025-02-18 20:41:11 +0800

    Final Modify code

--- practice01.php
+++ practice01.php
@@ -3,25 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Three Sides Calculator</title>
+    <title>Three Sides Calculator - Santos</title>
 </head>
 <body>
-    
-    <h2>Three Sides Calculator</h2>
-
     <?php 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $s1 = $_POST['side1'];
             $s2 = $_POST['side2'];
             $s3 = $_POST['side3'];
+
+            $s = ($s1 + $s2 + $s3) / 2;
+            $area = sqrt($s * ($s - $s1) * ($s - $s2) * ($s - $s3));
+
+            echo "<h2>Area: " . number_format($area, 2) . "</h2>";
         }
     ?>
 
-    <form action="POST" action="">
+    <h2>Three Sides Calculator</h2>
+    <form method="POST" action="">
         Side 1: <input type="number" name="side1" placeholder="Enter any Number" required><br><br>
         Side 2: <input type="number" name="side2" placeholder="Enter any Number" required><br><br>
         Side 3: <input type="number" name="side3" placeholder="Enter any Number" required><br><br>
-        <input type="submit" name="calculate" value="Calculate Area">
+        <button type="submit">Calculate</button>
     </form>
 </body>
 </html>
